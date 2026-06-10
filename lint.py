ce745e840654eda0ad643e4a20455b6c12e2fb13
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-06-10 10:58:36 +0200

    Add TSV and Markdown linting

--- lint.py
+++ lint.py
@@ -1,3 +1,11 @@
+# /// script
+# requires-python = ">=3.14"
+# dependencies = [
+#     "pandas>=3.0.3",
+# ]
+# ///
+
+import sys
 from pathlib import Path
 
 import pandas as pd
@@ -5,5 +13,11 @@ import pandas as pd
 HERE = Path(__file__).parent.resolve()
 
 if __name__ == '__main__':
+    fail = False
     for path in HERE.glob("*.tsv"):
-        pd.read_csv(path, sep="\t").to_csv(path, sep='\t', index=False)
+        original = path.read_text()
+        corrected = pd.read_csv(path, sep="\t").to_csv(sep='\t', index=False)
+        if original != corrected:
+            print(f"TSV not formatted properly: {path}")
+            fail = True
+    sys.exit(0 if fail else 1)
